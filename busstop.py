8230718ced2bd7b8e33fcc36b3f34b7612c3a7e2
Robin <robin.malmborg.te13@sodertalje.ntig.se>
2015-10-23 13:05:48 +0200

    Initial commit

---
+++ busstop.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+
+from trafiklab.sl import sites, trains
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def busstop():
+    return render_template('index.html', departurest=trains(9521), departuresb=(7571))
+
+
+#Site-ID
+@app.route('/sites')
+def sitessearch():
+
+    sitelist = sites(("Balticvagen"))
+
+    return    str(sitelist)
+
+
+if __name__ == '__main__':
+   # app.debug=True
+    app.run()

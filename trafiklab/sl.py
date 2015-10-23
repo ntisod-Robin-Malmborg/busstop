9b652b6f895f9aa7cb7fd843794568e08d15680b
Robin <robin.malmborg.te13@sodertalje.ntig.se>
2015-10-23 13:12:38 +0200

    Initial commit

---
+++ trafiklab/sl.py
@@ -0,0 +1,33 @@
+import requests
+
+from settings import key_plats,key_real
+#from datetime import datetime
+
+
+def sites(searchstring):
+
+        url = "http://api.sl.se/api2/typeahead.json?key={key}&searchstring={searchstring}"
+        #download page from trafiklab
+        resp = requests.get(url.format(key = key_plats, searchstring = searchstring))
+        return resp.json()['ResponseData']
+
+def departures(siteid):
+
+        url = "http://api.sl.se/api2/realtimedepartures.json?key={key}&siteid={siteid}&timewindow=60"
+
+        resp = requests.get(url.format(key = key_real, siteid = siteid))
+
+
+        return resp.json()['ResponseData']
+
+def trains(siteid):
+    trainlist=[]
+
+    return   departures(siteid)['Trains']
+
+def buses(siteid):
+   buslist=[]
+
+   return   departures(siteid)['Buses']
+
+
